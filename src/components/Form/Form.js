d3042e60388e9c5d7269b896a734e0dfb7733997
nikita <nikita.ratkevich91@mail.ru>
2023-02-06 04:13:50 +0200

    port

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -33,7 +33,7 @@ const Form = ({moveBack}) => {
       queryId
     }
 
-    fetch('https://192.168.0.109:8000/web-data', {
+    fetch('http://37.200.70.244:1924/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
